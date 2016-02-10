13881c308cbbc44704aad0e66a1422599a99c117
Tim Newbold <timnewbold.research@gmail.com>
2016-02-10 15:45:38 +0000

    Python code to process population data

--- DataChecks/3_CheckData2.py
+++ DataChecks/3_CheckData2.py
@@ -1,7 +1,27 @@
 
 inDir= "2_CheckData/"
 
+pd2005 = [None] * 212563217
+pd2010 = [None] * 212563217
+pd2015 = [None] * 212563217
+
+f = open(inDir + "nrows.txt",'r')
+f.readline()
+nrows = int(f.readline().replace('\n',''))
+f.close()
+
 f = open(inDir + "cell_data.csv",'r')
 
 h = f.readline().split(',')
 
+i = 0
+for x in range(0,nrows):
+    print(x)
+    l = f.readline().split(',')
+    if l[0] != 'NA':
+        pd2005[i] = l[0]
+        pd2010[i] = l[1]
+        pd2015[i] = l[2]
+        i=i+1
+    
+f.close()
